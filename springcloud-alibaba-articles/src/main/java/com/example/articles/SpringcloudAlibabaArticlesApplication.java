36cedd7ff0cbf52cb98d25b3f327f8e34462646e
heanhan <1763124707@qq.com>
2019-11-26 14:15:56 +0800

    微服务解决方案之-springcloud-aibaba ，使用alibaba的微服务的解决方案的学习，创建一个文章的微服务。

---
+++ springcloud-alibaba-articles/src/main/java/com/example/articles/SpringcloudAlibabaArticlesApplication.java
@@ -0,0 +1,13 @@
+package com.example.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAlibabaArticlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudAlibabaArticlesApplication.class, args);
+    }
+
+}
